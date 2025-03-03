19b32f4cdaa120f28fcca8241fd62b527e437bfd
sivasuthan manojan <mjcode6@gmail.com>
2025-03-03 15:28:01 +0100

    finnaly i work on transpose function so that's for neuralNetworkk

--- NeuralNet/Program.cs
+++ NeuralNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 using System.Runtime.CompilerServices;
 using System.Xml;
 
@@ -43,7 +44,7 @@ public class NeuralNetwork{
         for(int iteration = 0; iteration< numberOfIterations; iteration++){
             double[,] output = Think(trainingOutputs);
             double[,] error = PerformOperation(trainingOutputs, output, OPERATION.Subtract);
-            double[,] adjustement = DotProduct(Transpose(trainingInputs), performOperation(error, Activate(output, true), OPERATION.Multiply));
+            double[,] adjustement = DotProduct(Transpose(trainingInputs), PerformOperation(error, Activate(output, true), OPERATION.Multiply));
             weights = PerformOperation(weights,adjustement,OPERATION.Add);
         }
     }
@@ -75,12 +76,12 @@ public class NeuralNetwork{
 
      private double[,] PerformOperation(double[,] matrix1, double[,] matrix2, OPERATION operation){
         int rows = matrix1.GetLength(0);
-        int colums = matrix1.GetLength(1);
+        int columns = matrix1.GetLength(1);
 
-        double[,] result = new double[rows, colums];
+        double[,] result = new double[rows, columns];
 
         for(int i = 0; i< rows; i++){
-            for(int j = 0; j< colums; i++){
+            for(int j = 0; j< columns; j++){
                 switch(operation){
                     case OPERATION.Multiply:
                          result[i,j] = matrix1[i,j] * matrix2[i,j];
@@ -94,8 +95,58 @@ public class NeuralNetwork{
 
                 }
             }
-            return result;
+           
         }
+         return result;
      }
+
+   private double[,] Transpose(double[,] matrix)
+{
+    return matrix.Transpose(matrix.GetLength(0), matrix.GetLength(1));
+}
+
+
+static void Main(string[] args){
+NeuralNetwork neuralNetwork = new NeuralNetwork();
+double[,] trainingSetInput = new double[,] {{0,0,0}, {1,1,1}, {1,0,0}};
+double[,] trainingSetOutput = new double[,] {{0}, {1}, {1}};
+neuralNetwork.Train(trainingSetInput, trainingSetOutput, 1000);
+
+// Correcting the invocation: Calling a method explicitly
+double[,] output = neuralNetwork.Predict(new double[,] {{0,1,0}, {0,0,0}, {0,0,1}});
+
+PrintMatrix(output);
+
+
+
+}
+
+    private double[,] Predict(double[,] doubles)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void PrintMatrix(double[,] matrix){
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for(int row = 0; row < rows; row++){
+        for(int column = 0; column < columns; column++){
+            Console.Write(Math.Round(matrix[row, column]) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
 }  
 
+public static class Extensions{
+   public static double[,] Transpose(this double[,] array, int rows, int columns){
+     double[,] result = new double[columns,rows];
+     for(int row = 0; row<rows; row++){
+        for(int column = 0; column < columns; column++){
+            result[column,row] = array[row , column]; // correct indexing 
+        }
+     }
+     return result;
+   }
+}
